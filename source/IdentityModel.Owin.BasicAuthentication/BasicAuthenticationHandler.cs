acf5ad4e86ab28e4a6202d8a57dd9040851fb9f2
Dominick Baier <dbaier@leastprivilege.com>
2015-04-16 12:14:04 +0200

    migration

---
+++ source/IdentityModel.Owin.BasicAuthentication/BasicAuthenticationHandler.cs
@@ -0,0 +1,103 @@
+﻿/*
+ * Copyright 2014, 2015 Dominick Baier, Brock Allen
+ * (contributed by Pedro Felix)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityModel.Owin.BasicAuthentication
+{
+    class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    {
+        private readonly string _challenge;
+
+        public BasicAuthenticationHandler(BasicAuthenticationOptions options)
+        {
+            _challenge = "Basic realm=" + options.Realm;
+        }
+
+        protected override async Task<AuthenticationTicket> AuthenticateCoreAsync()
+        {
+            var authzValue = Request.Headers.Get("Authorization");
+            if (string.IsNullOrEmpty(authzValue) || !authzValue.StartsWith("Basic ", StringComparison.OrdinalIgnoreCase))
+            {
+                return null;
+            }
+            
+            var token = authzValue.Substring("Basic ".Length).Trim();
+            var claims = await TryGetPrincipalFromBasicCredentials(token, Options.CredentialValidationFunction);
+
+            if (claims == null)
+            {
+                return null;
+            }
+            else
+            {
+                var id = new ClaimsIdentity(claims, Options.AuthenticationType);
+                return new AuthenticationTicket(id, new AuthenticationProperties());
+            }
+        }
+
+        protected override Task ApplyResponseChallengeAsync()
+        {
+            if (Response.StatusCode == 401)
+            {
+                var challenge = Helper.LookupChallenge(Options.AuthenticationType, Options.AuthenticationMode);
+                if (challenge != null)
+                {
+                    Response.Headers.AppendValues("WWW-Authenticate", _challenge);
+                }
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        async Task<IEnumerable<Claim>> TryGetPrincipalFromBasicCredentials(string credentials,
+            BasicAuthenticationMiddleware.CredentialValidationFunction validate)
+        {
+            string pair;
+            try
+            {
+                pair = Encoding.UTF8.GetString(
+                    Convert.FromBase64String(credentials));
+            }
+            catch (FormatException)
+            {
+                return null;
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+
+            var ix = pair.IndexOf(':');
+            if (ix == -1)
+            {
+                return null;
+            }
+
+            var username = pair.Substring(0, ix);
+            var pw = pair.Substring(ix + 1);
+            
+            return await validate(username, pw);
+        }
+    }
+}
